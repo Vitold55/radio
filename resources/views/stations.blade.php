cd036aaaeffe40492663bc74c69a59ed50331090
Vilan <vitold55@ukr.net>
2015-10-27 10:52:55 +0200

    Stations

---
+++ resources/views/stations.blade.php
@@ -0,0 +1,7 @@
+@extends('app')
+
+@section('content')
+    Best FM - <audio src="http://radio.bestfm.fm:8080/bestfm_mp3" preload="auto" controls></audio><br />
+    Hit FM - <audio src="http://online-hitfm.tavrmedia.ua/HitFM" preload="auto" controls></audio><br />
+    Perets FM - <audio src="http://radio.stilnoe.fm:8080/radio-stilnoe" preload="auto" controls></audio><br />
+@stop
