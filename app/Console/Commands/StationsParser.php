23ad80e80789f3bbc0da476536df67f16675efa3
Vilan <vitold55@ukr.net>
2015-11-03 12:03:12 +0200

    Parser finish

--- app/Console/Commands/StationsParser.php
+++ app/Console/Commands/StationsParser.php
@@ -3,7 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use GuzzleHttp\Client;
+use App\Tasks\StationsParserTask;
 
 class StationsParser extends Command
 {
@@ -13,6 +13,17 @@ class StationsParser extends Command
      * @var string
      */
     protected $signature = 'parser:stations';
+
+    protected $stationsUrl = [
+        'ukr' => [
+            'url' => "http://lovi.fm/category/76/",
+            'countryId' => 1
+        ],
+        'rus' => [
+            'url' => "http://lovi.fm/category/68/",
+            'countryId' => 2
+        ],
+    ];
 
     /**
      * The console command description.
@@ -28,34 +39,15 @@ class StationsParser extends Command
 
     /**
      * Execute the console command.
-     *
-     * @return mixed
      */
     public function handle()
     {
-        $this->parseStations();
-    }
-
-    private function parseStations() {
-        $client = new Client();
-        $response = $client->get("http://lovi.fm/category/76/");
-
-        $body = $response->getBody()->getContents();
-
-        preg_match_all('/data-link=\".*\"/', $body, $ulBlock);
-
-        foreach($ulBlock[0] as $station) {
-            preg_match('/http[^\"]*/', $station, $source);
-
-            preg_match('/name=".*[^\"]*/', $station, $fullName);
-            preg_match('/[A-ZА-Я][^\-\"|\d|(]*/', $fullName[0], $name);
-            preg_match('/\d[^\s]*/', $fullName[0], $frequency);
-            preg_match('/\(\w*[^\)]*/', $fullName[0], $city);
-
-            $stationSource = $source[0];
-            $stationName = trim($name[0]);
-            $stationFrequency = !empty($frequency) ? $frequency[0] : null;
-            $stationCity = !empty($city) ? substr($city[0], 1) : null;
+        try {
+            $parserTask = new StationsParserTask();
+            $parserTask->parseStations($this->stationsUrl['ukr']);
+        }
+        catch (\Exception $e) {
+            echo $e->getTrace();
         }
     }
 }
---
+++ app/Tasks/StationsParserTask.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Tasks;
+
+use GuzzleHttp\Client;
+
+class StationsParserTask
+{
+    public function parseStations(array $stationsUrl) {
+        $client = new Client();
+        $response = $client->get($stationsUrl['url']);
+
+        $body = $response->getBody()->getContents();
+
+        preg_match_all('/data-link=\".*\"/', $body, $ulBlock);
+
+        $rating = 1;
+
+        foreach($ulBlock[0] as $station) {
+            preg_match('/http[^\"]*/', $station, $source);
+
+            preg_match('/name=".*[^\"]*/', $station, $fullName);
+            preg_match('/[A-ZА-Я][^\-\"|\d|(]*/', $fullName[0], $name);
+            preg_match('/\d[^\s|F]*/', $fullName[0], $frequency);
+            preg_match('/\(\w*[^\)]*/', $fullName[0], $city);
+
+            $stationSource = $source[0];
+            $stationName = trim($name[0]);
+            $stationFrequency = !empty($frequency) ? $frequency[0] : null;
+            $stationCity = !empty($city) ? substr($city[0], 1) : null;
+
+            $result = \DB::table('stations')->insert([
+                'source' => $stationSource,
+                'name' => $stationName,
+                'frequency' => $stationFrequency,
+                'city' => $stationCity,
+                'rating' => $rating,
+                'country' => $stationsUrl['countryId']
+            ]);
+
+            if (isset($result)) {
+                echo "Insert station " . $stationName . "\r\n";
+                $rating++;
+            }
+        }
+
+        echo "\r\n" . "Finish parsing!" . "\r\n\r\n";
+    }
+}
