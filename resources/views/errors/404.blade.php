330246972b11344dd0f41c658700a000dc761400
vilan <vitold55@ukr.net>
2016-05-24 15:22:33 +0300

    Add 404 Page not found

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+<head>
+    <title>404 - Page not found</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <link href="<?=asset('/assets/bootstrap/css/bootstrap.css')?>" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center; margin-top: 15%;">
+                <img src="<?=asset('/assets/images/verstka/404.jpg')?>" alt="404 Page not found" class="img-responsive" style="display: inline-block;">
+            </div>
+        </div>
+    </div>
+</body>
