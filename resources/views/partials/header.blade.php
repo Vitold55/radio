e1a0b4a3d4189d65b546c9eec4d60d16a2144f09
vilan <vitold55@ukr.net>
2016-05-26 18:09:04 +0300

    Add meta tags to main page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,7 @@
 <head>
     <title>FMka - онлайн радіо без меж</title>
+    <meta name="description" content="На сайті можна безкоштовно і без реєстрації слухати радіо онлайн!">
+    <meta name="keywords" content="радіо, радіо онлайн, слухати радіо онлайн, фм радіо онлайн, радіо онлайн україна, слухать радіо онлайн, радио онлайн, радио, слушать радио, слушать радио онлайн, слушать радио онлайн бесплатно, радио онлайн бесплатно, радио онлайн без регистрации, радиостанции, радио Украина, радио онлайн Украина, радиостанции Украины, FM, фм, online radio, radio">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <link href="<?=asset('/assets/bootstrap/css/bootstrap.css')?>" rel="stylesheet" type="text/css">
