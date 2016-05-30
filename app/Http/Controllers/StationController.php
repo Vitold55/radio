3506366d4d3f7be1977c7bec7dc0877064ab7daa
Vilan <vitold55@ukr.net>
2016-05-30 23:59:04 +0300

    Add categories and stations to admin panel

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -11,21 +11,19 @@ class StationController extends CrudController{
     public function all($entity){
         parent::all($entity); 
 
-        /** Simple code of  filter and grid part , List of all fields here : http://laravelpanel.com/docs/master/crud-fields
+		$this->filter = \DataFilter::source(new \App\Station);
+		$this->filter->add('name', 'Name', 'text');
+		$this->filter->submit('search');
+		$this->filter->reset('reset');
+		$this->filter->build();
 
-
-			$this->filter = \DataFilter::source(new \App\Category);
-			$this->filter->add('name', 'Name', 'text');
-			$this->filter->submit('search');
-			$this->filter->reset('reset');
-			$this->filter->build();
-
-			$this->grid = \DataGrid::source($this->filter);
-			$this->grid->add('name', 'Name');
-			$this->grid->add('code', 'Code');
-			$this->addStylesToGrid();
-
-        */
+		$this->grid = \DataGrid::source($this->filter);
+		$this->grid->add('id', 'Id');
+		$this->grid->add('name', 'Name');
+		$this->grid->add('source', 'Source');
+		$this->grid->add('rating', 'Rating');
+		$this->grid->add('available', 'Available');
+		$this->addStylesToGrid();
                  
         return $this->returnView();
     }
@@ -34,18 +32,16 @@ class StationController extends CrudController{
         
         parent::edit($entity);
 
-        /* Simple code of  edit part , List of all fields here : http://laravelpanel.com/docs/master/crud-fields
-	
-			$this->edit = \DataEdit::source(new \App\Category());
-
-			$this->edit->label('Edit Category');
-
-			$this->edit->add('name', 'Name', 'text');
-		
-			$this->edit->add('code', 'Code', 'text')->rule('required');
-
-
-        */
+		$this->edit = \DataEdit::source(new \App\Station());
+		$this->edit->label('Add /Edit station');
+		$this->edit->add('id', 'Id', 'text');
+		$this->edit->add('name', 'Name', 'text')->rule('required');
+		$this->edit->add('source', 'Source', 'text')->rule('required');
+		$this->edit->add('frequency', 'Frequency', 'text');
+		$this->edit->add('city', 'City', 'text');
+		$this->edit->add('rating', 'Rating', 'text');
+		$this->edit->add('logo', 'Logo', 'image')->move('assets/images/logos')->preview(100,80);
+		$this->edit->add('available', 'Available', 'checkbox');
        
         return $this->returnEditView();
     }    
