e8b090d2bad61e4fae9955d3a3a5ca96049d5fe9
vilan <vitold55@ukr.net>
2016-06-17 16:08:07 +0300

    Add styles for content text

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
 <div class="prefooter"></div>
 <footer>
-    &copy; <?php $year = date("Y", time()); echo $year > 2016 ? "2016-" . $year : 2016 ?> FMka.in.ua
+    &copy; <?php $year = date("Y", time()); echo $year > 2016 ? "2016-" . $year : 2016 ?> <a href="http://fmka.in.ua" class="footer-link">FMka.in.ua</a>
 </footer>
